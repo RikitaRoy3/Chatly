aab6a562c454fde87acaee0cb9966a2dee720458
Rikita Roy <rikitaroy21@gmail.com>
2026-01-20 02:17:57 +0530

    Changes done making Ready for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 import user_route from "./routes/user.route.js";
 import message_route from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT||5000;
@@ -14,6 +17,16 @@ const PORT = process.env.PORT||5000;
 app.use("api/routes",user_route );
 app.use("api/messages",message_route );
 
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
