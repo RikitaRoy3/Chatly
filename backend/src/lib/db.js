83f9dc1011cc3e2d75da350515b39b18641bec6d
Rikita Roy <rikitaroy21@gmail.com>
2026-01-19 12:49:58 +0530

    Database configuration done

---
+++ backend/src/lib/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    const  MONGO_URI = process.env.MONGO_URI;
+    if (!MONGO_URI) throw new Error("Sorry, please set your MONGO_URI first.");
+
+    const conn = await mongoose.connect(MONGO_URI);
+    console.log("MONGODB CONNECTED:", conn.connection.host);
+  } catch (error) {
+    console.error("Error connection to MONGODB:", error);
+    process.exit(1); // 1 status code means fail, 0 means success
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import user_route from "./routes/user.route.js";
 import message_route from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 
 dotenv.config();
@@ -16,4 +17,5 @@ app.use("api/messages",message_route );
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
+  connectDB();
 });
